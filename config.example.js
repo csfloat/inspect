6faf1ae98845b605224dfdee9baddddc54265384
Bradley Walters <zerfgog202@gmail.com>
2016-12-29 19:09:42 -0700

    Replace config json file with js file
    This allows us to use comments and opens the possibility for dynamic configuration

--- config.example.js
+++ config.example.js
@@ -1,8 +1,10 @@
-{
+module.exports = {
+	// Configuration for the HTTP API server
 	"http": {
 		"enable": true,
 		"port": 1739
 	},
+	// Configuration for the HTTPS API server
 	"https": {
 		"enable": false,
 		"port": 1738,
@@ -10,10 +12,12 @@
 		"cert_path": "certs/api.example.com.crt",
 		"ca_path": "certs/example.cer"
 	},
+	// Configuration for the socket.io server
 	"socketio": {
 		"enable": false,
 		"origins": "example.com:80"
 	},
+	// List of usernames and passwords for the Steam accounts
 	"logins": [
 		{
 			"user": "USERNAME",
@@ -26,10 +30,11 @@
 			"auth": "2FA_TOKEN_2"
 		}
 	],
+	// Amount of attempts for each request to Valve
 	"max_attempts": 1,
-	"max_attempts_comment": "Amount of attempts for each request to Valve",
+	// Amount of milliseconds to wait between subsequent requests to Valve (per bot)
 	"request_delay": 1100,
-	"request_delay_comment": "Amount of milliseconds to wait between subsequent requests to Valve (per bot)",
+	// Origins allowed to connect to the HTTP/HTTPS API
 	"allowed_origins": [
 		"http://example.com",
 		"https://example.com",
@@ -37,6 +42,7 @@
 		"http://steamcommunity.com",
 		"https://steamcommunity.com"
 	],
+	// Redis DB connection info
 	"database": {
 		"url": "localhost",
 		"port": 27017
