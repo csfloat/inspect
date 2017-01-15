c24d2d0c9fe8f4cecaa0144779f3dc73662aa610
Step7750 <Step7750@gmail.com>
2017-01-15 15:31:52 -0700

    Inspect URL Edge Case Fixes and Param Obj Parsing
    Ensures we parse a built inspect URL from parameters to ensure proper
    types, structure, etc...

--- inspect_url.js
+++ inspect_url.js
@@ -1,18 +1,38 @@
 class InspectURL {
     constructor() {
+        const params = ["s", "a", "d", "m"];
         this.properParams = false;
 
-    	if (arguments.length == 1) {
+    	if (arguments.length == 1 && typeof arguments[0] === "string") {
     		// parse the inspect link
     		this.parseLink(arguments[0]);
     	}
-    	else if (arguments.length == 4) {
-            this.s = arguments[0];
-            this.a = arguments[1];
-            this.d = arguments[2];
-            this.m = arguments[3];
+        else if (arguments.length == 1 && typeof arguments[0] === "object") {
+            // parse object with the params
+            let amt = 0;
 
-            this.properParams = true;
+            for (let param of params) {
+                if (arguments[0][param] && typeof arguments[0][param] === "string" && arguments[0][param].length > 0) {
+                    this[param] = arguments[0][param];
+                    amt++;
+                }
+                else this[param] = "0";
+            }
+
+            if (amt === 3) this.parseLink(this.buildLink());
+        }
+    	else if (arguments.length == 4) {
+            // parse each arg
+
+            // Ensure each arg is a string
+            for (let param in params) {
+                if (typeof arguments[param] === "string") {
+                    this[params[param]] = arguments[param];
+                }
+                else return;
+            }
+
+            this.parseLink(this.buildLink());
     	}
     }
 
@@ -21,11 +41,11 @@ class InspectURL {
         var groups = /steam:\/\/rungame\/730\/\d+\/[+ ]csgo_econ_action_preview ([SM])(\d+)A(\d+)D(\d+)/.exec(link);
 
         if (groups) {
-            if (groups[1] == "S") {
+            if (groups[1] === "S") {
                 this.s = groups[2];
                 this.m = "0";
             }
-            else if (groups[1] == "M") {
+            else if (groups[1] === "M") {
                 this.m = groups[2];
                 this.s = "0";
             }
@@ -43,9 +63,14 @@ class InspectURL {
     }
 
     getLink() {
-        if (!this.properParams) return;
+        if (this.properParams) return this.buildLink();
+        else return;
+    }
 
-        if (this.s == "0") return "steam://rungame/730/76561202255233023/+csgo_econ_action_preview M" + this.m + "A" + this.a + "D" + this.d;
+    buildLink() {
+        if (!("a" in this && "d" in this && ("m" in this || "s" in this))) return;
+
+        if (this.s === "0" && this.m) return "steam://rungame/730/76561202255233023/+csgo_econ_action_preview M" + this.m + "A" + this.a + "D" + this.d;
         else return "steam://rungame/730/76561202255233023/+csgo_econ_action_preview S" + this.s + "A" + this.a + "D" + this.d;
     }
 }
