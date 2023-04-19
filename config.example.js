efd02c85b84817270403993425c1896fc28074fa
Step7750 <step7750@gmail.com>
2023-04-18 23:29:43 -0600

    Clarifies Example Proxies

--- config.example.js
+++ config.example.js
@@ -19,7 +19,7 @@ module.exports = {
             'auth': '2FA_TOKEN_2'
         }
     ],
-    // Optional proxies to auto-rotate for each bot, each bot will have a random proxy
+    // Optional HTTP/SOCKS5 proxies to auto-rotate for each bot, each bot will have a random proxy
     'proxies': [],
     // Bot settings
     'bot_settings': {
