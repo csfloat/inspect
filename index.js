e66fc73b20f210a66e15914f99d2be61e74eea64
maxim.shnyagin <mshnyagin@evrone.com>
2024-02-19 19:49:18 +0300

    proxy-checker

--- index.js
+++ index.js
@@ -69,7 +69,7 @@ app.use(function (req, res, next) {
   }
   next();
 });
-app.use(bodyParser.json({ limit: "5mb" }));
+app.use(bodyParser.json({ limit: "500mb" }));
 
 app.use(function (error, req, res, next) {
   // Handle bodyParser errors
--- test.js
+++ test.js
@@ -6,21 +6,93 @@ const utils = require("./lib/utils");
 const winston = require("winston");
 const proxyAgent = require('https-proxy-agent');
 
-async function checkProxies(proxies) {
-  return proxies.filter(async proxy => {
-    const [ip, port, login, password] = proxy.split(':');
-    const proxyUrl = `http://${login}:${password}@${ip}:${port}`;
-    const agent = new proxyAgent.HttpsProxyAgent(proxyUrl);
+const http = require('http');
 
-    try {
-      console.log(`check proxy: ${proxy}`);
-      const response = await fetch('https://steamcommunity.com', { agent });
-      return response.ok;
-    } catch (error) {
-      console.log(`Proxy failed: ${proxy}`, error.message);
-      return false;
+const errors = [
+  'Bad proxy string',
+  'Proxy offline'
+];
+
+const proxy_check = p => {
+  return new Promise((resolve, reject) => {
+
+    let proxy = {
+      host: '',
+      port: 0,
+      proxyAuth: ''
+    };
+
+    if (typeof p === 'object') {
+      if (Array.isArray(p)) {
+        if (typeof p[0] === 'object') {
+          proxy = p[0];
+        } else if (typeof p === 'string') {
+          p = p[0];
+        } else {
+          return reject(errors[0]);
+        }
+      } else {
+        proxy = p;
+      }
     }
+
+    if (typeof p === 'string') {
+      if (p.indexOf('@') + 1) {
+        proxy.proxyAuth = p.split('@')[0];
+        const host_port = p.split('@')[1];
+        if (host_port.indexOf(':') + 1) {
+          proxy.host = host_port.split(':')[0];
+          proxy.port = host_port.split(':')[1];
+        }
+      } else {
+        if (p.indexOf(':') + 1) {
+          proxy.host = p.split(':')[0];
+          proxy.port = p.split(':')[1];
+        }
+      }
+    }
+
+    const proxy_options = {
+      method: 'CONNECT',
+      path: 'www.google.com:443',
+      timeout: 1000,
+      agent: false
+    };
+
+    if (proxy.host) {
+      proxy_options.host = proxy.host;
+    }
+    if (proxy.port) {
+      proxy_options.port = proxy.port;
+    }
+    if (proxy.proxyAuth) {
+      proxy_options.headers = {
+        'Proxy-Authorization': 'Basic ' + Buffer.from(proxy.proxyAuth).toString('base64')
+      };
+    }
+
+    const req = http.request(proxy_options);
+    req.on('connect', res => {
+      req.destroy();
+      if (res.statusCode === 200) {
+        return resolve(p);
+      } else {
+        return reject(errors[1]);
+      }
+    });
+    req.on('timeout', () => {
+      req.destroy();
+    });
+    req.on('error', err => {
+      return reject((err && err.code) || errors[1]);
+    });
+    req.end();
+
   });
+}
+
+async function checkProxies(proxies) {
+  return await Promise.allSettled(proxies.map(p => proxy_check(p)));
 }
 
 async function checkAndWriteProxies() {
@@ -28,13 +100,31 @@ async function checkAndWriteProxies() {
     .readFileSync("travchis.txt")
     .toString()
     .split(/\n|\r\n/)
-    .filter(line => line.length);
+    .filter(line => line.length)
+    .map(str => {
+      const [ip, port, login, pass] = str.split(":");
+      return {
+        host: ip,
+        port: parseInt(port),
+        proxyAuth: `${login}:${pass}`
+      };
+    });
 
-  const healthProxies = await checkProxies(proxies);
-  fs.writeFileSync("proxies.txt", proxies.join("\n"));
+  const checked = await checkProxies(proxies);
+
+  const healthProxies = checked
+    .filter(res => res.status !== 'rejected')
+    .map(obj => `${obj.value.host}:${obj.value.port}:${obj.value.proxyAuth.split(":")[0]}:${obj.value.proxyAuth.split(":")[1]}`);
+
+  fs.writeFileSync("proxies.txt", healthProxies.join("\n"));
 }
 
 async function main() {
+  await checkAndWriteProxies();
+  await resetProxies();
+}
+
+async function resetProxies() {
   const proxies = fs
     .readFileSync("proxies.txt")
     .toString()
